fb2a15347d20034c87a2c955ab19b1d78afc6285
Hyeonsoo Ko <150876969+hyeonsooko@users.noreply.github.com>
2024-08-15 15:23:46 -0500

    token

--- backend/api/resources/user.py
+++ backend/api/resources/user.py
@@ -1,6 +1,15 @@
 from api.models.user import UserModel
 from flask_restful import Resource, request
 from werkzeug.security import generate_password_hash
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt_identity,
+    get_jwt,
+    jwt_required,
+)
+from flask.views import MethodView
+from werkzeug.security import check_password_hash
 from api.schemas.user import UserRegisterSchema
 register_schema = UserRegisterSchema()
 
@@ -28,3 +37,16 @@ class UserRegister(Resource):
                 )
             user.save_to_db()
             return {"success": f"{user.username}!"}, 201
+        
+class UserLogin(MethodView):
+    
+    def post(self):
+        data = request.get_json()
+        user = UserModel.find_by_email(data["email"])
+        
+        if user and check_password_hash(user.password, data["password"]):
+            access_token = create_access_token(identity=user.username, fresh=True)
+            refresh_token = create_refresh_token(identity=user.username)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+        
+        return {"Unauthorized": "Check your email and password"}, 401
