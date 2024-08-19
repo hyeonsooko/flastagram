4599b84065fc90cc36311f9c3f891645d0dae07e
Hyeonsoo Ko <150876969+hyeonsooko@users.noreply.github.com>
2024-08-19 03:41:54 -0500

    server.js

---
+++ frontend/server/server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "..")));
+
+app.listen(3000, (err) => {
+    if (err) return console.log(err);
+    console.log("The server is listening to port 3000");
+});
+
+app.get("/flastagram/posts", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "index.html"));
+});
